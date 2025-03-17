695a3d67e7429aa16e00a2332b3637d4c11cd09f
Heimvik <cmheimvik@gmail.com>
2025-03-17 21:15:42 +0900

    Working as it should on sim

--- lab2/DMAC/RTL/DMAC_CFG.sv
+++ lab2/DMAC/RTL/DMAC_CFG.sv
@@ -52,11 +52,12 @@ module DMAC_CFG
     output  reg     [15:0]      byte_len_o,
     output  wire                start_o,
     input   wire                done_i
-);
+    );
     import DMAC_CFG_UTILS::*;
     cfg_reg_t cfg;
-    logic error;
-    
+    logic memory_error = 0;
+    logic start_error = 0;
+    logic [`DATA_WIDTH-1:0] rdata;
 
     //----------------------------------------------------------
     // INIT
@@ -69,6 +70,8 @@ module DMAC_CFG
         cfg.dma_len = 0;
         cfg.dma_cmd = 0;
         cfg.dma_status = `DATA_WIDTH'h1;
+        memory_error = 0;
+        rdata = 0;
     end
     
     //----------------------------------------------------------
@@ -90,26 +93,54 @@ module DMAC_CFG
     // paddr   :    |DMA_CMD|
     // pwdata  :    |   1   |
     // start   : _______----_____________________________
+    
+    /*
+    ---Signaling start of a new DMA opeartion---
+    The requirements for issuing a new DMA transfer is: a write, a psel for valid values and a not an ongoing transfer
+    the right adress in the select phase, and the DMA_CMD must be issued to start the transfer
+    Signaling the end of a DMA operation is done by done_i and lies in the FSM logic, not here.
+    */
+    logic start_avalibale;
+    wire start;
+    assign start = start_avalibale && penable_i;
+    always_comb begin
+        if(psel_i && pwrite_i && (paddr_i & `DMA_BASE+`DMA_CMD_OFFSET) && (pwdata_i & 1)) begin
+            if(cfg.dma_status[0]) begin
+                start_avalibale = 1;
+                start_error = 0;
+            end else begin
+                start_avalibale = 0;
+                start_error = 1;
+            end         
+        end else begin
+            start_avalibale = 0;
+        end
+    end
 
-    // Intermediate signal to indiacate when a write (2. phase) can occur (Our condition for having a write)
+
+    // Intermediate signal to indicate when a write (2. phase) can occur (Our condition for having a write)
     wire wren;
     assign wren = psel_i && penable_i && pwrite_i;
     always @(posedge clk) begin
         //If our condition for write is true, clock in pwdata_i into the correct memory location (remember the offset)
         if(wren) begin
             case(paddr_i)
-                `DMA_BASE+`DMA_VER_OFFSET: cfg.dma_ver = pwdata_i; //Write restricted?
-                `DMA_BASE+`DMA_SRC_OFFSET: cfg.dma_src = pwdata_i;
-                `DMA_BASE+`DMA_DST_OFFSET: cfg.dma_dst = pwdata_i;
-                `DMA_BASE+`DMA_LEN_OFFSET: cfg.dma_len = pwdata_i;
-                `DMA_BASE+`DMA_CMD_OFFSET: cfg.dma_cmd = pwdata_i;
-                default: error = 1;
+                `DMA_BASE+`DMA_SRC_OFFSET: cfg.dma_src <= pwdata_i;
+                `DMA_BASE+`DMA_DST_OFFSET: cfg.dma_dst <= pwdata_i;
+                `DMA_BASE+`DMA_LEN_OFFSET: cfg.dma_len <= pwdata_i;
+                `DMA_BASE+`DMA_CMD_OFFSET: cfg.dma_cmd <= pwdata_i;
+                default: memory_error <= 1;
             endcase
+        end
+        //Conditions for writing to the cfg.dma_status register is that it should be 0 when start_o is issued, and 1 when done_i is issued
+        if(start && cfg.dma_status[0]) begin
+            cfg.dma_status <= 0;
+        end
+        if(done_i && !cfg.dma_status[0]) begin
+            cfg.dma_status <= 1;
         end
     end
 
-    wire start;
-    assign start = (paddr_i && `DMA_BASE+`DMA_CMD_OFFSET) && (pwdata_i & 1) && penable_i;
 
     
     //----------------------------------------------------------
@@ -127,32 +158,24 @@ module DMAC_CFG
     
 
     //Only setup the buffer value in the setup period (whenever we have psel_i and not penable)
-    logic [`DATA_WIDTH-1:0] rdata;
     wire rden;
     assign rden = psel_i && !penable_i && !pwrite_i;
     always @(posedge clk) begin
         if(rden) begin
             case(paddr_i)
-                `DMA_BASE+`DMA_VER_OFFSET: rdata = cfg.dma_ver;
-                `DMA_BASE+`DMA_SRC_OFFSET: rdata = cfg.dma_src;
-                `DMA_BASE+`DMA_DST_OFFSET: rdata = cfg.dma_dst;
-                `DMA_BASE+`DMA_LEN_OFFSET: rdata = cfg.dma_len;
-                `DMA_BASE+`DMA_CMD_OFFSET: rdata = cfg.dma_cmd;
-                `DMA_BASE+`DMA_STATUS_OFFSET: rdata = cfg.dma_status;
-                default: error = 1;
+                `DMA_BASE+`DMA_VER_OFFSET: rdata <= cfg.dma_ver;
+                `DMA_BASE+`DMA_SRC_OFFSET: rdata <= cfg.dma_src;
+                `DMA_BASE+`DMA_DST_OFFSET: rdata <= cfg.dma_dst;
+                `DMA_BASE+`DMA_LEN_OFFSET: rdata <= cfg.dma_len;
+                `DMA_BASE+`DMA_STATUS_OFFSET: rdata <= cfg.dma_status;
+                default: memory_error <= 1;
             endcase
         end
     end
 
-    //----------------------------------------------------------
-    // ERROR HANFLING
-    //----------------------------------------------------------
-    //Alignemnt, reserved region writes, checking, etc
-
-
     assign  pready_o            = 1'b1;
     assign  prdata_o            = rdata;
-    assign  pslverr_o           = error;
+    assign  pslverr_o           = memory_error || start_error;
 
     assign  src_addr_o          = cfg.dma_src;
     assign  dst_addr_o          = cfg.dma_dst;
